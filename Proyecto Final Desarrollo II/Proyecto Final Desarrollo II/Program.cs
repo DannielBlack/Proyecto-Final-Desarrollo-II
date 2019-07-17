870313d0adba742b4ccc897958d03129f5034985
Sergio Danniel Ordoñez <sergio.ordonez@udla.edu.ec>
2019-07-17 17:26:15 -0500

    hola programa

---
+++ Proyecto Final Desarrollo II/Proyecto Final Desarrollo II/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Final_Desarrollo_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
